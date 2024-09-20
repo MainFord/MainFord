0fc45be3e63b7348995c84267328053ea4eabd12
MainFord <mainfordapp@gmail.com>
2024-09-20 03:09:32 +0000

    update;

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-import db from './config/db.js'
+import db from './config/db.js';
 
 dotenv.config();
 
-db()
+db();
 
 const app = express();
 
 app.use(express.json());
-app.get('/',(re,res)=>{res.json({"success":true})})
+
+// Sample route
+app.get('/', (req, res) => { 
+    res.json({ "success": true }); 
+});
+
+// User and Admin Routes
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
+
+// Handle unhandled routes
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Not Found' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Server Error '+err.stack });
+});
 
 export default app;
