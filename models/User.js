64b74f4d4463bf2fe98c9cf546eb31adeafb98cf
MainFord <mainfordapp@gmail.com>
2024-10-01 05:49:42 +0000

    up

--- models/User.js
+++ models/User.js
@@ -13,37 +13,47 @@ const userSchema = new mongoose.Schema({
       type: String, 
       get: decrypt, 
       set: encrypt, 
-      default: ''  // Default value set to an empty string
+      default: ''  
     },
     ifsc: { 
       type: String, 
       get: decrypt, 
       set: encrypt, 
-      default: ''  // Default value set to an empty string
+      default: ''  
     },
     holderName: { 
       type: String, 
       get: decrypt, 
       set: encrypt, 
-      default: ''  // Default value set to an empty string
+      default: ''  
     },
   },
-  
-  photoUrl: { type: String, default:'https://ideogram.ai/assets/progressive-image/balanced/response/6z5HqcytSX2hHcE3AxKUBg' }, // Cloudinary image URL
+  photoUrl: { 
+    type: String, 
+    default:'https://ideogram.ai/assets/progressive-image/balanced/response/6z5HqcytSX2hHcE3AxKUBg' 
+  },
   adminApproved: { type: Boolean, default: false },
-  referralCode: { type: String, unique: true, required: true }, // Unique referral code
+  referralCode: { type: String, unique: true, required: true }, 
   referredBy: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'User',
-    default: null  // Default value set to null
+    default: null  
   },
-  paymentUrlOfReg: {type: String, required:true},
+  paymentUrlOfReg: { type: String, required:true },
   paymentHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Payment' }],
-  courses : [{
-    url1: { type: String , default: 'PLfqMhTWNBTe137I_EPQd34TsgV6IO55pt' },
-    url2: { type: String , default: 'PLXwTOG3-tRwiJmAyVJ47SVvv-dUIy2S0I' },
-    url3: { type: String , default: 'PLXwTOG3-tRwgy4lJ9j_CPwpJmr2uCaGH1' },
-  }]
+  courses: {
+    type: [{
+      url1: { type: String, default: 'PLfqMhTWNBTe137I_EPQd34TsgV6IO55pt' },
+      url2: { type: String, default: 'PLXwTOG3-tRwiJmAyVJ47SVvv-dUIy2S0I' },
+      url3: { type: String, default: 'PLXwTOG3-tRwgy4lJ9j_CPwpJmr2uCaGH1' },
+    }],
+    default: [{
+      url1: 'PLfqMhTWNBTe137I_EPQd34TsgV6IO55pt',
+      url2: 'PLXwTOG3-tRwiJmAyVJ47SVvv-dUIy2S0I',
+      url3: 'PLXwTOG3-tRwgy4lJ9j_CPwpJmr2uCaGH1',
+    }]
+  }
 }, { timestamps: true });
+
 
 export default mongoose.model('User', userSchema);
