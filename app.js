ebc0593985ff7670d311a8e2b0caccf6e0e75f41
MainFord <mainfordapp@gmail.com>
2024-10-23 05:33:54 +0000

    osjfj

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(cors({
     origin: 'https://musical-rotary-phone-g47x75gr6g4w395rw-3000.app.github.dev', // Replace with your frontend URL
     credentials: true, // Allow cookies to be sent
+    exposedHeaders: ['Content-Range'],
 }));
 
 // Use cookie-parser middleware
